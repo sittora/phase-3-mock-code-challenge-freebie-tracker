5fe6362dbd5858afad8f1c25062b322cba16cb84
sittora <sittora@gmail.com>
2021-09-28 16:12:39 -0400

    Assignment completed

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,15 @@
+
+
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def give_freebie(dev, item_name, value)
+        Freebie.create(dev: dev, item_name: item_name, value: value, company: self)
+    end
+
+    def self.oldest_company
+        Company.all.order(:founding_year).first
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,20 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazoprpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+Freebie.create(item_name: "Sticker", value: 5, company_id: google.id, dev_id: rick.id)
+Freebie.create(item_name: "Bottle", value: 8, company_id: facebook.id, dev_id: morty.id)
+Freebie.create(item_name: "Pen", value: 3, company_id: dunder.id, dev_id: meseeks.id)
+Freebie.create(item_name: "Book", value: 10, company_id: enron.id, dev_id: gazoprpazop.id)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
